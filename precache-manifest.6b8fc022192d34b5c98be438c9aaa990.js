f3cce6b61e91d18b9fee850231f7d7e439a880b8
amanrawat16 <amanrawat9916@gmail.com>
2021-06-04 15:48:24 +0530

    Updates

---
+++ precache-manifest.6b8fc022192d34b5c98be438c9aaa990.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccfa91069fae6449706965b074e3316",
+    "url": "/reactapp2/index.html"
+  },
+  {
+    "revision": "628853ecc3657cd40b47",
+    "url": "/reactapp2/static/js/2.3e668906.chunk.js"
+  },
+  {
+    "revision": "a548e228a3c61e8e7290d7162635b1c7",
+    "url": "/reactapp2/static/js/2.3e668906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd8616d79e6e1964a12",
+    "url": "/reactapp2/static/js/main.d8341328.chunk.js"
+  },
+  {
+    "revision": "9d82e57c10f0b97afbe8",
+    "url": "/reactapp2/static/js/runtime-main.f45efcf7.js"
+  }
+]);
